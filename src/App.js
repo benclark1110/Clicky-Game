a2e4ba7880bad7a9a84987d2fdf19b920ecfa6c5
benclark1110 <benclark1110@gmail.com>
2019-07-14 23:39:16 -0400

    adding user images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
+import ImageCard from "./components/Cards";
+import friends from "./images.json";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Jumbotron />
+      <ImageCard
+        image={friends[0].image}
+      />
+      <ImageCard
+        image={friends[1].image}
+      />
+      <ImageCard
+        image={friends[2].image}
+      />
     </div>
   );
 }
