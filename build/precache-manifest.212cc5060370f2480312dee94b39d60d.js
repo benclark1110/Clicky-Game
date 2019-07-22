5b283eb97368447cc467c4d29063b75526797ac0
benclark1110 <benclark1110@gmail.com>
2019-07-22 00:17:56 -0400

    deploy ready

---
+++ build/precache-manifest.212cc5060370f2480312dee94b39d60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0a335994cc48f19d427ff5598462b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d86fc3d0d6960caa66",
+    "url": "/static/css/main.a2f1729c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "80d86fc3d0d6960caa66",
+    "url": "/static/js/main.79674108.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
